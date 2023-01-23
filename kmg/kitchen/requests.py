bb11d86f51a56c907bf90f15b031b9d6b4c82c10
Kevin M Granger <git@kevinmgranger.me>
2023-01-23 17:28:16 -0500

    add requests bearer auth

--- kmg/kitchen/requests.py
+++ kmg/kitchen/requests.py
@@ -1,6 +1,19 @@
 import requests
+from requests.auth import AuthBase
 
 
 def checked(resp: requests.Response) -> requests.Response:
     resp.raise_for_status()
     return resp
+
+
+AUTHORIZATION_HEADER = "Authorization"
+
+
+class BearerAuth(AuthBase):
+    def __init__(self, token: str):
+        self.token = token
+
+    def __call__(self, request: requests.Request) -> requests.Request:
+        request.headers[AUTHORIZATION_HEADER] = f"Bearer {self.token}"
+        return request
